388635f1663fb0b4bc994713b499e0744aae4c84
aarush goyal <goyalaarush232@gmail.com>
2026-06-09 01:19:16 +0530

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int find(int x, vector<int> &par){
+        while(par[x] != x){
+            par[x] = par[par[x]];
+            x = par[x];
+        }
+        return x;
+    }
+    void merge(int x, int y, vector<int> &par, int &count){
+        int rx = find(x, par);
+        int ry = find(y, par);
+        if(rx == ry){
+            return ;
+        }
+        par[ry] = rx;
+        count--;
+    }
+
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<int> par(n);
+        iota(par.begin(), par.end(),0);
+        unordered_set<int> s;
+        int count = n;
+        vector<vector<int>> adj(n);
+        for(auto v : edges){
+            if(find(v[0], par) != find(v[1], par)){
+                merge(v[0],v[1],par, count);
+            }
+            else{
+                return false;
+            }
+        }
+        // for(int i = 0; i < n; i++){
+        //     s.insert(par[i]);
+        // }
+
+        // if(s.size() == 1){
+        //     return true;
+        // }
+        cout << count;
+        if(count == 1){
+            return true;
+        }
+        return false;
+    }
+};
