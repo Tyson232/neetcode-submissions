400fe801c319bdfc67f9632736dbf508eda093ef
aarush goyal <goyalaarush232@gmail.com>
2026-08-10 16:28:36 +0530

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // int globalMax = 1, globalMin = 1
+    // void solve(int i, int &globalMax, vector<int>& nums){
+    //     if(i >= nums.size()){
+    //         return ;
+    //     }
+    //     int localm = 1, localmin = 1;
+    //     for(int i = )
+
+        
+
+    // }
+    int maxProduct(vector<int>& nums) {
+        int globalMax = INT_MIN;
+        int localm = 1, localmin = 1;
+        for(int i = 0; i < nums.size(); i++){
+            int a = localm*nums[i];
+            int b = localmin*nums[i];
+            localm = max({a,b,nums[i]});
+            localmin = min({a,b,nums[i]});
+           // cout << "localm " << localm << ", localmin " << localmin << ", globalMax " << globalMax << "\n";
+            globalMax = max(localm,globalMax);
+
+        }
+        return globalMax;
+    }
+};
