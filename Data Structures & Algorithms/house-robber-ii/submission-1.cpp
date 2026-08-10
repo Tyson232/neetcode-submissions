72fb5fcd5669772f96de4f8870926a9768aceaa3
aarush goyal <goyalaarush232@gmail.com>
2026-08-10 16:02:05 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        return max(robLine(nums, 0, n - 2), robLine(nums, 1, n - 1));
+    }
+private:
+    int robLine(vector<int>& nums, int l, int r) {
+        int prev = 0, cur = 0;          // prev = best up to i-2, cur = best up to i-1
+        for (int i = l; i <= r; i++) {
+            int t = max(cur, prev + nums[i]);
+            prev = cur;
+            cur = t;
+        }
+        return cur;
+    }
+};
